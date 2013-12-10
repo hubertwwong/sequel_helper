43a3d5e726e089e708206ca6f6de5e8314f1cde7
Hubert Wong <hubertwwong@gmail.com>
2013-12-09 20:14:08 -0800

    figured out that I don't need array of hash.

--- lib/sequel_helper.rb
+++ lib/sequel_helper.rb
@@ -21,7 +21,7 @@ class SequelHelper
     @host = params.fetch(:host)
     @user = params.fetch(:user)
     @password = params.fetch(:password)
-    @database = params.fetch(:db_name)
+    @database = params.fetch(:database)
     
     # @client is the sequel object.
     # when its connected.
@@ -35,39 +35,51 @@ class SequelHelper
   # SELECT methods
   ############################################################################
   
-  def select(params = {})
-    return "pie"
-  end
+  #def select(params = {})
+  #  puts "KAHAN..................."
+  #  return "pie"
+  #end
   
+  # QUERY methods
+  ############################################################################
+  
+  # checks if a row with a given constraint exist.
+  def row_exist?(table_name, where_param)
+    # note that you need the all so it actually runs
+    # this applies for all queries for sequel.
+    result = @client.from(table_name).where(where_param).all
+    
+    if result == nil || result.length == 0
+      #puts 'row does not exist'
+      return false
+    else
+      #puts 'row exist'
+      return true
+    end
+  end
   
   # MISC METHODS
   ############################################################################
   
+  # MIGHT NOT BE NEEDED...
   # convert sequel results to an array of hashes.
   # in sequel, results are in its own data struct.
   # if you call all rows, it returns as an array of hashes.
   # if you do a where call, you can call this on the results
   # so it will return it as an array of hashes.
-  def to_array_of_hashes(sequel_results)
-    if sequel_results == nil
-      return nil
-    else
-      result = Array.new
-      sequel_results.each do |item|
-        result.push(item)  
-      end
-      return result
-    end
-  end
+  #def to_array_of_hashes(sequel_results)
+  #  if sequel_results == nil
+  #    return nil
+  #  else
+  #    result = Array.new
+  #    sequel_results.each do |item|
+  #      result.push(item)  
+  #    end
+  #    return result
+  #  end
+  #end
 
-  # inject_db_params
-  # might not nbe needed.
-  def inject_db_params(params = {})
-    injected_params = params
-    injected_params[:database] = @database
-    
-    return injected_params
-  end
+
   
   # connections
   ############################################################################
@@ -85,7 +97,7 @@ class SequelHelper
   # might need to call this if the one in the mysql2 client
   # is not fast enough.
   def close
-    @client
+    @client.close
   end
   
   # TEMP
--- spec/features/sequel_helper_spec.rb
+++ spec/features/sequel_helper_spec.rb
@@ -2,9 +2,72 @@ require_relative "../../lib/sequel_helper"
 
 describe SequelHelper do
 
+  # testing some stuff out with sequel... can ignore this.
+  describe "RAW" do
+    before(:each) do
+      @db_cred = {
+        :adapter => "mysql2",
+        :host => "localhost",
+        :database => "space_ship",
+        :user => "root",
+        :password => "password"
+      }
+      @db_table_name = "fleet"
+    end
+    
+    describe ".all test" do
+      it "returns an array" do
+        sq = SequelHelper.new @db_cred
+        result = sq.client.from(:fleet).all
+        expect(result.instance_of?(Array)).to eq(true)
+      end
+      
+      it "1st item is a hash" do
+        sq = SequelHelper.new @db_cred
+        result = sq.client.from(:fleet).all
+        expect(result[0].instance_of?(Hash)).to eq(true)
+      end
+    end
+  end
+
+  describe "QUERY" do
+    before(:each) do
+      @db_cred = {
+        :adapter => "mysql2",
+        :host => "localhost",
+        :database => "space_ship",
+        :user => "root",
+        :password => "password"
+      }
+      @db_table_name = "fleet"
+    end
+    
+    describe "row_exist?" do
+      it "foo should eq false" do
+        sq = SequelHelper.new @db_cred
+        expect(sq.row_exist?("fleet", :name => "foo")).to eq(false)
+      end
+      
+      it "name2 should eq true" do
+        sq = SequelHelper.new @db_cred
+        expect(sq.row_exist?("fleet", :name => "name2")).to eq(true)
+      end
+    end
+  end
+
   describe "hello" do
+    before(:each) do
+      @db_cred = {
+        :adapter => "mysql2",
+        :host => "localhost",
+        :database => "space_ship",
+        :user => "root",
+        :password => "password"
+      }
+    end
+    
     it "should return hello" do
-      ms = SequelHelper.new
+      ms = SequelHelper.new @db_cred
       expect(ms.hello).to eq('hello')
     end
   end
